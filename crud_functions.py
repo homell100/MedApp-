2afe9aeec18576822f311007adebc2757983a9cf
Sergi Justiniano Claramunt <sjustinianoclaramunt@gmail.com>
2022-11-08 18:03:26 +0100

    Adding patient pg function & query

--- crud_functions.py
+++ crud_functions.py
@@ -12,3 +12,15 @@ def insert_staff(name, surname, position):
 												 position = position))
 	conn.commit()
 	conn.close()
+
+def insert_patient(name, surname, entry_date, exit_date, id_bed):
+	conn = make_connection()
+	with conn.cursor() as cur:
+		cur.execute(q.QUERY_INSERT_PATIENT.format(name = name,
+													 surname = surname,
+													 entry_date=entry_date,
+													 exit_date=exit_date,
+													 id_bed=id_bed)
+		)
+	conn.commit()
+	conn.close()
--- queries.py
+++ queries.py
@@ -7,3 +7,8 @@ QUERY_INSERT_STAFF = """
 INSERT INTO staff(name, surname, position)
 	VALUES ('{name}', '{surname}', '{position}')
 """
+
+QUERY_INSERT_PATIENT = """
+INSERT INTO patient(name, surname, entry_date, exit_date, id_bed)
+	VALUES ('{name}', '{surname}', '{entry_date}', '{exit_date}', {id_bed})
+"""
