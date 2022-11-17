e6f0282b8855f7df09018c38cd3322bbe21af064
Sergi Justiniano Claramunt <sjustinianoclaramunt@gmail.com>
2022-11-17 17:51:50 +0100

    Adding name and columns names for staff_patient table

--- database_design.py
+++ database_design.py
@@ -1,10 +1,12 @@
 TABLE_NAME_PATIENT = "patient"
 TABLE_NAME_STAFF = "staff"
 TABLE_NAME_ROOM = "room"
+TABLE_NAME_STAFF_PATIENT = "staff_patient"
 
 TABLES_COLUMNS = {
 	TABLE_NAME_ROOM: ["floor", "number_room"],
 	TABLE_NAME_PATIENT: ["name", "surname", "entry_date", "exit_date", "id_bed"],
-	TABLE_NAME_STAFF: ["name", "surname", "position"]
+	TABLE_NAME_STAFF: ["name", "surname", "position"],
+	TABLE_NAME_STAFF_PATIENT: ["id_staff", "id_patient", "start_date", "end_date"]
 
 }
