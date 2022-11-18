0667e97a878c9c30ecac0d04bf0c1f940c0c818a
Sergi Justiniano Claramunt <sjustinianoclaramunt@gmail.com>
2022-11-18 13:41:56 +0100

    Adding list of tables where duplicates are accepted

--- database_design.py
+++ database_design.py
@@ -8,7 +8,8 @@ TABLES_COLUMNS = {
 	TABLE_NAME_ROOM: ["floor", "number_room"],
 	TABLE_NAME_PATIENT: ["name", "surname", "entry_date", "exit_date", "id_bed"],
 	TABLE_NAME_STAFF: ["name", "surname", "position"],
-	TABLE_NAME_BED: ["id_room"]
+	TABLE_NAME_BED: ["id_room"],
 	TABLE_NAME_STAFF_PATIENT: ["id_staff", "id_patient", "start_date", "end_date"]
-
 }
+
+LIST_TABLE_ACCEPT_DUPLICATES = [TABLE_NAME_BED]
