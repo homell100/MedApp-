1132b34266d3809ea3d46e25c4f17f31ba328491
Sergi Justiniano Claramunt <sjustinianoclaramunt@gmail.com>
2022-12-07 16:04:15 +0100

    Adding select routing functionality

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, redirect, request
 
 from routing.insert import insert_page
+from routing.select import select_page
+
 app = Flask(__name__)
 app.secret_key = "biiqw73_^¨PLAKV7tejas@~#2d"
 
 app.register_blueprint(insert_page)
+app.register_blueprint(select_page)
+
+
 @app.route("/home")
 @app.route("/")
 def index():
